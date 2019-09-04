d82b946a33cade049a1090cb1e08ca1c2058cbc1
gyogo <dfva1234@naver.com>
2019-09-05 08:59:42 +0900

    foods. drinks 활용 ★

---
+++ 패키지/main.drinks.py
@@ -0,0 +1,6 @@
+# from foods.drinks import milk, cola, water
+from foods.drinks import * #__init__은 *로 할 때만 거의 쓰임
+
+milk.drink()    # 꿀꺽꿀꺽~ 진하고 신선한 우유의 향연이~!
+cola.drink()    # 톡톡 캬아~ 역시 콜라는 정민이네 콜라
+water.drink()   # 맑고 투명한 물은 역시 지하수
