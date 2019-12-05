e560fc384612db42f1c719ce46b6ea9659ae71ba
gyogo <dfva1234@naver.com>
2019-12-05 10:10:59 +0900

    pyautogui

---
+++ IX 프로젝트/macro1.py
@@ -0,0 +1,12 @@
+import  pyautogui as pag
+
+if __name__ == '__main__':
+    pag.moveTo(189,452,duration=0.5)
+    # pag.click()
+    # pag.click()
+    # pag.click(clicks=2)
+    # pag.doubleClick()
+    # pag.move(100,200,duration=0.5)
+    # pag.rightClick() #마우스 오른쪽 버튼
+    # pag.click(button="right")
+    pag.drag(0,200,duration=0.5)
